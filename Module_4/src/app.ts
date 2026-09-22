eadae097612f4085140293a7220e49f1b04e86b9
Imtius10 <h.imtius10@gmail.com>
2026-09-22 14:47:34 +0600

    routes

--- Module_4/src/app.ts
+++ Module_4/src/app.ts
@@ -4,10 +4,12 @@ import express, {
   Response,
   urlencoded,
 } from "express";
+
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import globalError from "./middlewares/GlobalError/globalError";
+
 import { userRouter } from "./module/user/user.routes";
 import { propertyRouter } from "./module/properties/properties.routes";
 import { rentalRequestRouter } from "./module/rentalRequest/rentalrequest.routes";
@@ -15,7 +17,21 @@ import { landlordRouter } from "./module/landlord/landlord.routes";
 import { adminRouter } from "./module/admin/admin.routes";
 import { reviewRouter } from "./module/review/review.routes";
 
+import { paymentRouter } from "./module/payment/payment.routes";
+import { paymentWebhookController } from "./module/payment/payment.webhook.Controller";
+
 const app: Application = express();
+
+
+app.post(
+  "/api/payments/webhook",
+  express.raw({
+    type: "application/json",
+  }),
+  paymentWebhookController.stripeWebhook
+);
+
+
 
 app.use(express.json());
 
@@ -27,18 +43,54 @@ app.use(
 );
 
 app.use(urlencoded({ extended: true }));
+
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
-});
 
-app.use("/api/auth", userRouter);
-app.use("/api/properties", propertyRouter);
-app.use("/api/rentals", rentalRequestRouter);
-app.use("/api/landlord", landlordRouter);
-app.use("/api/admin", adminRouter);
-app.use("/api/reviews", reviewRouter);
+
+app.get(
+  "/",
+  (req: Request, res: Response) => {
+    res.send("Hello");
+  }
+);
+
+
+
+app.use(
+  "/api/auth",
+  userRouter
+);
+
+app.use(
+  "/api/properties",
+  propertyRouter
+);
+
+app.use(
+  "/api/rentals",
+  rentalRequestRouter
+);
+
+app.use(
+  "/api/landlord",
+  landlordRouter
+);
+
+app.use(
+  "/api/admin",
+  adminRouter
+);
+
+app.use(
+  "/api/reviews",
+  reviewRouter
+);
+
+app.use(
+  "/api/payments",
+  paymentRouter
+);
 
 
 app.use(globalError);
---
+++ Module_4/src/module/payment/payment.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+
+import { UserRole } from "../../../generated/prisma/client";
+import { auth } from "../../middlewares/auth/auth";
+
+import { paymentController } from "./payment.Controller";
+
+const router = Router();
+
+/**
+ * Create Stripe Checkout Session
+ */
+router.post(
+  "/create",
+  auth(UserRole.TENANT),
+  paymentController.createPayment
+);
+
+/**
+ * Tenant payment history
+ */
+router.get(
+  "/my-payments",
+  auth(UserRole.TENANT),
+  paymentController.getMyPayments
+);
+
+/**
+ * Get one payment
+ */
+router.get(
+  "/:id",
+  auth(UserRole.TENANT),
+  paymentController.getPaymentById
+);
+
+export const paymentRouter = router;
